3bf2ff9ef254dc9d575043903cc514a1db2eeca2
Mario252003 <ux21ii039@ux.edu.mx>
2024-07-29 01:56:03 -0600

    w
    ww

--- build/lib/PaqueteExtraOpt/__init__.py
+++ build/lib/PaqueteExtraOpt/__init__.py
@@ -1,16 +1,16 @@
-from .MetodosMultivariables.randomwalked import caminata_aleatoria
-from .MetodosMultivariables.cauchy import cauchy
-from .MetodosMultivariables.Reeves import fletcherReeves
-from .MetodosMultivariables.HookeJeeves import hooke_jeeves
-from .MetodosMultivariables.NelderMead import nelder_mead
-from .MetodosMultivariables.newton import newton
+from MetodosMultivariables.caminataAleatoria import caminata_aleatoria
+from MetodosMultivariables.cauchy import cauchy
+from MetodosMultivariables.fletcherReeves import fletcherReeves
+from MetodosMultivariables.hookeJeeves import hooke_jeeves
+from MetodosMultivariables.nelderMeadSimplex import nelder_mead
+from MetodosMultivariables.newton import newton
 
-from .MetodosUnivariables.Fibonacci import fibonacci_search
-from .MetodosUnivariables.biseccion import biseccion
-from .MetodosUnivariables.BusquedaDorada import busquedaDorada
-from .MetodosUnivariables.IntervalHalving import interval_halving_method
-from .MetodosUnivariables.Secante import secante
-from .MetodosUnivariables.NewtonRaph import newton_raphson
+from MetodosUnivariables.Fibonacci import fibonacci_search
+from MetodosUnivariables.biseccion import biseccion
+from MetodosUnivariables.BusquedaDorada import busquedaDorada
+from MetodosUnivariables.IntervalHalving import interval_halving_method
+from MetodosUnivariables.Secante import secante
+from MetodosUnivariables.NewtonRaph import newton_raphson
 
-from .FuncionesPrueba.FuncionesMultivariables import ackley, rastrigin, himmelblau, sphere, shekel, threehumpcamel, styblinskitang, beale, booth, bunkin, crossintray, easom, eggholder, goldstein, holdertable, levi, matyas, mccormick, rosenbrock, schaffer2, schaffer_n4
-from .FuncionesPrueba.FuncionesUnivariables import f1, f2, f3, f4
+from FuncionesPrueba.FuncionesMultivariables import ackley, rastrigin, himmelblau, sphere, shekel, threehumpcamel, styblinskitang, beale, booth, bunkin, crossintray, easom, eggholder, goldstein, holdertable, levi, matyas, mccormick, rosenbrock, schaffer2, schaffer_n4
+from FuncionesPrueba.FuncionesUnivariables import f1, f2, f3, f4
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
        # el nombre debe coincidir con el nombre de la carpeta 	  
        #'modulomuysimple'
         name="PaqueteExtraOpt", 
-        version="0.2",
+        version="0.3",
         author="Mario Zepeda",
         author_email="<marioraul.zepeda@hotmail.com>",
         description=DESCRIPTION,
